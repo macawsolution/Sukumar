867e74fb9df7d412f5007997dc1f5e8ad9cecc5b
logarajae <logarajae@gmail.com>
2022-03-30 09:02:37 +0530

    initial

---
+++ CorrespondenceSystem/UICorrespondenceSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace UICorrespondenceSystem
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-min.js"));
+             
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ CorrespondenceSystem/UICorrespondenceSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UICorrespondenceSystem.MvcApplication" Language="C#" %>
