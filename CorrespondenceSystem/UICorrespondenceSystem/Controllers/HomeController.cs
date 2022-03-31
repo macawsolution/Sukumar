69629ea354abe62648fe42700605622454b026ba
logarajae <logarajae@gmail.com>
2022-03-31 07:46:50 +0530

    second push

--- CorrespondenceSystem/UICorrespondenceSystem/Controllers/HomeController.cs
+++ CorrespondenceSystem/UICorrespondenceSystem/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UICorrespondenceSystem.DataAccess;
+using UICorrespondenceSystem.Models;
+using Correspondence = UICorrespondenceSystem.Models.Correspondence;
 
 namespace UICorrespondenceSystem.Controllers
 {
     public class HomeController : Controller
     {
+        COR_DEVEntities db = new COR_DEVEntities();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,49 @@ namespace UICorrespondenceSystem.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult New()
+        {
+            ViewBag.Message = "New Correspondence page";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Correspondence correspondence)
+        {
+            DataAccess.Correspondence obj = new DataAccess.Correspondence();
+            obj.Serial_Number = correspondence.Serial_Number;
+            obj.Subject = correspondence.Subject;
+            obj.Sender_Creation_Date = correspondence.Correspondence_Date;
+            obj.Received_Date = correspondence.Received_Date;
+            obj.Sender_Type = Convert.ToByte(correspondence.Sender_Type);
+            db.Correspondences.Add(obj);
+            db.SaveChanges();
+            return View(correspondence);
+        }
+
+        [HttpPost]
+        public JsonResult Index(string Prefix)
+        {
+            //Note : you can bind same list from database  
+            List<Organization> ObjList = new List<Organization>()
+            {
+
+                new Organization {Id=1,Name="Org1" },
+                new Organization {Id=2,Name="Org12" },
+                new Organization {Id=3,Name="Org13" },
+                new Organization {Id=4,Name="Org14" },
+                new Organization {Id=5,Name="Org15" },
+                new Organization {Id=6,Name="Org16" },
+                new Organization {Id=7,Name="Org17" }
+
+        };
+            //Searching records from list using LINQ query  
+            var Name = (from N in ObjList
+                        where N.Name.ToLower().Contains(Prefix.ToLower())
+                        select new { N.Name });
+            return Json(Name, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ CorrespondenceSystem/UICorrespondenceSystem/Models/Correspondence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UICorrespondenceSystem.Models
+{
+    public class Correspondence
+    {
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+
+
+        [Display(Name = "Correspondence Serial Number")]
+        [Required(ErrorMessage ="Please enter serial number!.")]
+        public string Serial_Number { get; set; }
+
+        [Display(Name ="Correspondence Date")]
+        [Required(ErrorMessage = "Please enter/select correspondence date!.")]
+        [DataType(DataType.Date)] 
+
+        public DateTime Correspondence_Date { get; set; }
+
+        [Display(Name ="Received Date")] 
+        [DataType(DataType.Date)] 
+
+        [Required(ErrorMessage = "Please enter/select received date!.")]
+        public DateTime Received_Date { get; set; }
+
+        [Display(Name ="Subject")]
+        [Required(ErrorMessage = "Please enter subject!.")]
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public int Sender_Type { get; set; }
+        public int Sender_Internal_ID { get; set; }
+        public int Sender_External_ID { get; set; }
+        public int Expected_Reply_WithIn_Days { get; set; }
+
+    }
+
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
